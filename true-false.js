f6b9886a62174123c8ea82a1652b04edb54a4099
abutalha08 <taufique752@gmail.com>
2025-06-19 01:35:04 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,155 @@
+//false hoy____
+// 0
+//""
+//undefined
+//null
+//NaN
+//false
+//eshob hole
+
+//Truthy
+//"0"
+//" "
+// []
+//{}
+
+
+// only 0 bade javascript zekono number true hishebe dhore nibe
+
+const age = 4;
+if(age>0){
+  console.log("Condition is true");
+
+}
+else{
+  console.log("Condition is false");
+}
+
+const age2 = -4;
+if(age2){
+  console.log("Condition is true");
+
+}
+else{
+  console.log("Condition is false");
+}
+
+const age3 = 0;
+if(age3){
+  console.log("Condition is true");
+
+}
+else{
+  console.log("Condition is false");
+}
+
+//empty string chara baki shob string true
+const Name = "Talha";
+if(Name.length>0){
+  console.log("Condition is true");
+}
+else{
+  console.log("Condition is false");
+}
+
+
+//example 2
+
+const Name2 = "Talha";
+if(Name2.length){
+  console.log("Condition is true");
+}
+else{
+  console.log("Condition is false");
+}
+
+//example 3
+
+const Name3 = "Talha";
+if(Name3){
+  console.log("Condition is true");
+}
+else{
+  console.log("Condition is false");
+}
+
+//Example 4
+const Name4 = "0";
+if(Name4){
+  console.log("Condition is true");
+}
+else{
+  console.log("Condition is false");
+}
+
+//Example 5
+const Name5 = "";
+if(Name5){
+  console.log("Condition is true");
+}
+else{
+  console.log("Condition is false");
+}
+//Example 6
+const Name6 = " ";
+if(Name6){
+  console.log("Condition is true");
+}
+else{
+  console.log("Condition is false");
+}
+
+//Example 7
+//jodi variable er value  declare kora na thake
+
+let name7;
+console.log(name7);
+
+if(name7){
+  console.log("Condition is true");
+}
+else{
+  console.log("Condition is false");
+}
+
+//example 8 
+//variable a value jodi "null" set kora thake then,
+
+let name8 = null;
+console.log(name8);
+
+if(name8){
+  console.log("Condition is true");
+}
+else{
+  console.log("Condition is false");
+}
+
+//example 9
+let name9 = NaN;
+console.log(name9);
+
+if(name9){
+  console.log("Condition is true");
+}
+else{
+  console.log("Condition is false");
+}
+//example 10
+let name10 = false;
+
+if(name10){
+  console.log("Condition is true");
+}
+else{
+  console.log("Condition is false");
+}
+//example 11
+let name11 = 'false';
+
+if(name11){
+  console.log("Condition is true");
+}
+else{
+  console.log("Condition is false");
+}
