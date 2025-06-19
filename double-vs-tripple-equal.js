69be10898d11c0c01830f64551935e73413bd856
abutalha08 <taufique752@gmail.com>
2025-06-19 13:57:38 +0600

    learned about double vs tripple equal

---
+++ double-vs-tripple-equal.js
@@ -0,0 +1,71 @@
+//double equal mane shudu value takee chake korbe.value ta ki type sheta check korbe na.
+const first = 2;
+const second = 4;
+if(first==second){
+  console.log("condition is true");
+}
+else{
+  console.log("condition is false");
+}
+
+//example 2
+const first1 = 2;
+const second1 = 2;
+if(first1==second1){
+  console.log("condition is true");
+}
+else{
+  console.log("condition is false");
+}
+//example 3
+const first2 = 2; //number 
+const second2 = "2";//string
+if(first2==second2){
+  console.log("condition is true");
+}
+else{
+  console.log("condition is false");
+}
+
+//Tripple equal mane value and type 2 tai check korbe
+const one = 2; //number
+const two = "2"; //string
+if(one===two){
+  console.log("condition is true");
+}
+else{
+  console.log("condition is false");
+}
+
+
+//double equal korle jodi 2 ta type same na thake tahole kono ek take dhore shey same type er korar try korbe tarpor ogula ke compare korbe.
+const me = 2;
+const you = true;
+if(me==you){
+  console.log("condition is true");
+}
+else{
+  console.log("condition is false");
+}
+
+
+const me1 = 1;
+const you1 = true; // here true means 1
+if(me1==you1){
+  console.log("condition is true");
+}
+else{
+  console.log("condition is false");
+}
+
+//js e kono ekta value ache true sheti compare kortise 1 er shathe
+//abar false compare kortise 0 er shathe.
+
+const me2 = 1;
+const you2 = false; // here false means 0
+if(me2==you2){
+  console.log("condition is true");
+}
+else{
+  console.log("condition is false");
+}
